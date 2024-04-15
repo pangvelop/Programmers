bdc57e95f9dd50ccc67ece361fe57cc3747dc835
kingo555 <107342259+kingo555@users.noreply.github.com>
2024-04-15 22:05:10 +0900

    [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,20 @@
+def solution(code):
+    mode = 0
+    ret = ''
+    for i in range(len(code)) :
+        if mode == 0 : 
+            if code[i] != '1' and i%2==0:
+                ret += code[i]
+            elif code[i] == '1' :
+                mode = 1
+        
+        else :
+            if code[i] != '1' and i%2==1:
+                ret += code[i]
+            elif code[i] == '1' :
+                mode = 0
+    if ret=='' :
+        return "EMPTY"
+    else :
+        return ret
+        
