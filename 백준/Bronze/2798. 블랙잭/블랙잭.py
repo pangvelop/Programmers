3dafddfbffc6220de257e2b647ccb489de3217af
김광오 <107342259+kingo555@users.noreply.github.com>
2024-10-05 23:40:15 +0900

    [Bronze II] Title: 블랙잭, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+max = 0
+for i in range(n-2) :
+    for j in range(i+1,n-1) :
+        for k in range(j+1,n) :
+            sum = num[i]+num[j]+num[k]
+            if (sum<=m and sum>max) :
+                max = sum
+print(max)    
