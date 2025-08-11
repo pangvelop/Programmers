8480c3e5b3b9a16778de07cdd7b8128fc5b7672c
김광오 <107342259+pangvelop@users.noreply.github.com>
2025-08-11 15:23:35 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.print(Math.max(A, B));
+
+    }
+}
